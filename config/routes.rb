240fea6dedeb5fdb77c9573bfc1d597737ffb30a
桜井俊介 <tuktuk.032@gmail.com>
2018-10-06 21:03:50 +0900

    problem scaffold 追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :problems
   devise_for :users
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/migrate/20181005150907_devise_create_users.rb
+++ db/migrate/20181005150907_devise_create_users.rb
@@ -4,8 +4,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string  :name,               null: false, default: ""
+      t.text    :icon,               null: false, default: ""
+      t.text    :profile
+      t.integer :role
+      t.integer :prefecture_code
+      t.string  :email,              null: false, default: ""
+      t.string  :encrypted_password, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
@@ -38,7 +43,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
+    add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
 end
