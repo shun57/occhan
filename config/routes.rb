2786a5c6d8c395b4bfe55e26944566253af1cfd5
桜井俊介 <tuktuk.032@gmail.com>
2018-10-08 00:37:48 +0900

    userマイページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
   root 'problems#index'
   resources :problems
   devise_for :users
+  resources :users, :only => [:show]
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
