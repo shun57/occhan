73823c62775857098ae1b69f6748bef203d35c1e
桜井俊介 <tuktuk.032@gmail.com>
2018-10-16 23:21:09 +0900

    users_controllerにbefore_actionユーザーログイン制御をつけた

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  
   def show
     @user = User.find(params[:id])
     @request_problems = @user.request_management_problems
