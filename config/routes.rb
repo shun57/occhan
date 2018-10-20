9498e77e9698732ba87bdb3f4aee77db100def0c
桜井俊介 <tuktuk.032@gmail.com>
2018-10-20 13:28:51 +0900

    アプリセットコンパイル

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   resources :conversations do
     resources :messages
   end
-  mount LetterOpenerWeb::Engine, at: '/letter_opener'
+  # mount LetterOpenerWeb::Engine, at: '/letter_opener'
 end
