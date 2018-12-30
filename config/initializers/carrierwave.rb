58d54a2d3eaba0a1b370ce3a13f3ad88d7a15f5f
桜井俊介 <tuktuk.032@gmail.com>
2018-12-30 15:42:59 +0900

    S3 herokuの設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    region: 'ap-northeast-1'
+    aws_access_key_id: ENV["S3_ACCESS_KEY"],
+    aws_secret_access_key: ENV["S3_SECRET_KEY"],
+    region: ENV["S3_REGION"]
   }
 
   config.fog_directory  = 'rails-photo-saku'
