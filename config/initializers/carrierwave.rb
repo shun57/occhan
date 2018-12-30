194016d1065463af7d7028664cb64383c8ddadb2
桜井俊介 <tuktuk.032@gmail.com>
2018-12-30 17:29:54 +0900

    s3用の設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,16 +7,18 @@ if Rails.env.production?
     config.cache_storage = :fog
     config.cache_dir = "#{Rails.root}/tmp/uploads"
     config.fog_provider = 'fog/aws'
+    config.fog_public = true
 
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY'],
-      region: ENV['S3_REGION']
+      region: ENV['S3_REGION'],
+      path_style: true
     }
 
     config.fog_directory  = ENV['S3_BUCKET']
     config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/rails-photo-saku"
-    CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
   end
+      CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 end
